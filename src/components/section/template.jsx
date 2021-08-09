8f0e95c68477ff0c641602d94b9716ff363d8c7e
Lucio Benini <iiriya@iiriya.com>
2021-08-09 22:44:51 +0200

    Update template.jsx

--- src/components/section/template.jsx
+++ src/components/section/template.jsx
@@ -1,18 +1,21 @@
 import React from "react"
 
 import Content from "./content"
+/*
 import Cook from "./template/cook"
 import Family from "./template/family"
 import Lady from "./template/lady"
-
+*/
 export default function Switcher({ content }) {
   switch (content.template) {
+    /*
     case "cook":
       return <Cook content={content} />
     case "family":
       return <Family content={content} />
     case "lady":
       return <Lady content={content} />
+      */
     default:
       return <Content content={content} />
   }
