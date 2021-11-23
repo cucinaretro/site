efb2f81a93ac20756a466a5c730afa53d6599239
Lucio Benini <iiriya@iiriya.com>
2021-11-23 10:49:00 +0100

    Update 404.jsx

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,16 +1,44 @@
 import React from "react"
+import { graphql } from "gatsby"
 import { Seo } from "@pittica/gatsby-plugin-seo"
+import { formatLocale } from "@pittica/gatsby-plugin-utils"
 
 import Footer from "../components/ui/footer"
 import Hero from "../components/ui/hero"
 import Main from "../components/ui/main"
 import TopMenu from "../components/ui/top-menu"
 
-export default function NotFoundPage({ children, location, locale }) {
+export default function NotFoundPage({
+  children,
+  location,
+  locale,
+  data: {
+    site: {
+      siteMetadata: {
+        organization: { phone },
+      },
+    },
+    navigation,
+  },
+}) {
+  const localeObject = formatLocale(locale)
+
+  localeObject.path =
+    localeObject.language === process.env.LOCALE
+      ? ""
+      : `/${localeObject.language}`
+
   return (
     <div>
       <Seo title="404" description="Not Found" path={location.pathname} />
-      <TopMenu title="404" location={location} locale={locale} />
+      <TopMenu
+        title="404"
+        start={navigation.start}
+        end={navigation.end}
+        location={location}
+        locale={localeObject}
+        phone={phone}
+      />
       <Main>
         <Hero title="404" subtitle="Not Found" />
         {children}
@@ -19,3 +47,55 @@ export default function NotFoundPage({ children, location, locale }) {
     </div>
   )
 }
+
+export const pageQuery = graphql`
+  query Page404 {
+    site {
+      siteMetadata {
+        organization {
+          phone
+        }
+      }
+    }
+    navigation: graphCmsNavigation(
+      slug: { eq: "top" }
+      stage: { eq: PUBLISHED }
+    ) {
+      title
+      start {
+        ... on GraphCMS_Page {
+          id
+          remoteTypeName
+          slug
+          pageTitle: title
+          remotePath
+        }
+        ... on GraphCMS_NavigationLink {
+          id
+          remoteTypeName
+          uri
+          title
+          sticky
+          icon
+        }
+      }
+      end {
+        ... on GraphCMS_Page {
+          id
+          remoteTypeName
+          slug
+          pageTitle: title
+          remotePath
+        }
+        ... on GraphCMS_NavigationLink {
+          id
+          remoteTypeName
+          uri
+          title
+          sticky
+          icon
+        }
+      }
+    }
+  }
+`
